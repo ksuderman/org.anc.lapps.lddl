0f51fdb616153abf1f87591e4b86e19beeccb539
Keith Suderman <suderman@anc.org>
2014-02-25 11:09:09 -0500

    Bumped version.

--- src/main/java/org/anc/lapps/lddl/Version.java
+++ src/main/java/org/anc/lapps/lddl/Version.java
@@ -6,7 +6,7 @@ package org.anc.lapps.lddl;
  */
 public final class Version
 {
-	private static final String version = "1.3.0-SNAPSHOT";
+	private static final String version = "1.3.0";
 
 	public static String getVersion() { return version; }
 }
