b676dfed5dfc462fc9400f63927d107551a90bee
Baian Liu <baliu@vassar.edu>
2016-04-01 15:19:47 -0400

    Added files via upload
    fixed #5

---
+++ lddl.bat
@@ -0,0 +1,10 @@
+:: Startup script for LDDL. Change the LDDL installation directory
+:: below if needed. This script should be in the same director as
+:: the .jar file.
+
+@ECHO OFF
+SET MEMSIZE=4G
+SET VERSION=1.3.4
+FOR %%F in (lddl-%VERSION%.jar) do set JAR=%%~$PATH:F
+
+java -jar %JAR% %*
